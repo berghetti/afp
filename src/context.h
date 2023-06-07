6f92f1bc12e28a8ca59cfcd374366d1ff68f385a
Mayco S. Berghetti <mayco.s.berghetti@gmail.com>
2023-06-07 15:04:54 +0000

    add likely alloc contexts

--- src/context.h
+++ src/context.h
@@ -1,23 +1,26 @@
 #ifndef CONTEXT_H
 #define CONTEXT_H
 
+#include <rte_branch_prediction.h>
 #include <rte_malloc.h>
 #include <sys/ucontext.h>
 #include <ucontext.h>
 
 #define STACK_SIZE 16 * 1024
 
+// TODO: create a mempool to this allocations
+
 static inline ucontext_t *
 context_alloc ( void )
 {
-  // TODO: create a mempool to this allocations
   ucontext_t *ctx;
   ctx = rte_malloc ( NULL, sizeof ( ucontext_t ), 0 );
-  if ( !ctx )
+  if ( unlikely ( !ctx ) )
     return NULL;
 
   void *stack = rte_malloc ( NULL, STACK_SIZE, 0 );
-  if ( !stack )
+  if ( unlikely ( !stack ) )
+
     {
       rte_free ( ctx );
       return NULL;
