31ac4988c508c99eb15a6ca0bb2ea7216a5c369d
Mayco S. Berghetti <mayco.s.berghetti@gmail.com>
2023-06-19 19:47:22 +0000

    clean signal_core test

--- tests/signal_core.c
+++ tests/signal_core.c
@@ -1,11 +1,4 @@
 
-/* Test overhead to send signals on same time from various threads.
- * Kernel taken lock to send signal, so us test misalign between threads
- * to send the signal to avoid taken the lock on same time.
- *
- * The 'us' parameter is the difference between threads starting o signal send.
- *
- */
 
 #define _GNU_SOURCE
 #include <pthread.h>
@@ -18,7 +11,6 @@
 #include <time.h>
 #include <sys/time.h>
 
-#include <sys/syscall.h> /* Definition of SYS_* constants */
 #include <unistd.h>
 #include <signal.h>
 #include <x86intrin.h>
@@ -32,14 +24,14 @@
 
 #include "util.h"
 
-#define SENDER_CORE 0
-#define WORKER_CORE 1
+#define SENDER_CORE 5
+#define WORKER_CORE 7
 
 #define RUNS 100000UL
 
 static volatile int worker_ready, worker_stop;
 static uint32_t cycles_by_us;
-static uint32_t samples[RUNS];
+static uint32_t samples[RUNS], timer_frequency[RUNS];
 static uint32_t tsc_worker[RUNS], tsc_sender_worker[RUNS];
 static uint64_t tsc_starts[RUNS], tsc_ends[RUNS];
 static uint64_t volatile sender_start_tsc, worker_start_tsc;
@@ -53,66 +45,25 @@ static pid_t worker_id;
 static void
 handler ( int sig )
 {
-  // usleep ( 5 );
+  static uint64_t last_now = 0;
   uint64_t now = __rdtsc ();
+
   tsc_sender_worker[i_handler] = now - sender_start_tsc;
   tsc_worker[i_handler] = now - worker_start_tsc;
+
+  timer_frequency[i_handler] = now - last_now;
+  last_now = now;
 
   i_handler++;
 
   wait_handler = 0;
 }
 
-static inline double
-cycles2us ( uint32_t cycles )
-{
-  return ( double ) cycles / cycles_by_us;
-}
-
-static void
-print ( uint32_t *samples, size_t size, char *msg )
-{
-  qsort ( samples, size, sizeof ( *samples ), cmp_uint32 );
-
-  uint32_t min, mean, p99, p999, max;
-  min = samples[0];
-  mean = percentile ( samples, size, 0.50f );
-  p99 = percentile ( samples, size, 0.99f );
-  p999 = percentile ( samples, size, 0.999f );
-  max = samples[size - 1];
-
-  printf ( "\n%s\n"
-           "  Min:   %u (%.2f us)\n"
-           "  50%%:   %u (%.2f us)\n"
-           "  99%%:   %u (%.2f us)\n"
-           "  99.9%%: %u (%.2f us)\n"
-           "  Max:   %u (%.2f us)\n",
-           msg,
-           min,
-           cycles2us ( min ),
-           mean,
-           cycles2us ( mean ),
-           p99,
-           cycles2us ( p99 ),
-           p999,
-           cycles2us ( p999 ),
-           max,
-           cycles2us ( max ) );
-}
-
 static void *
 worker ( void *arg )
 {
-  cpu_set_t cpus;
-  CPU_ZERO ( &cpus );
-  CPU_SET ( WORKER_CORE, &cpus );
-  if ( pthread_setaffinity_np ( pthread_self (), sizeof ( cpu_set_t ), &cpus ) )
-    {
-      printf ( "Could not pin thread to core %d.\n", WORKER_CORE );
-      exit ( 1 );
-    }
-
-  printf ( "Started worker thread on core %u\n", WORKER_CORE );
+  pin_to_cpu ( WORKER_CORE );
+  printf ( "Started worker thread on core %u\n", sched_getcpu () );
 
   worker_id = gettid ();
 
@@ -126,20 +77,11 @@ worker ( void *arg )
 static void
 sender ( void )
 {
-  cpu_set_t cpus;
-  CPU_ZERO ( &cpus );
-  CPU_SET ( SENDER_CORE, &cpus );
-  if ( pthread_setaffinity_np ( pthread_self (), sizeof ( cpu_set_t ), &cpus ) )
-    {
-      printf ( "Could not pin thread to core %d.\n", SENDER_CORE );
-      exit ( 1 );
-    }
-
-  printf ( "Started sender thread on core %u\n", SENDER_CORE );
+  pin_to_cpu ( SENDER_CORE );
+  printf ( "Started sender thread on core %u\n", sched_getcpu () );
 
   pid_t tgid = getpid ();
 
-  uint64_t end, start = __rdtsc ();
   for ( unsigned int i = 0; i < RUNS; i++ )
     {
       tsc_starts[i] = __rdtsc ();
@@ -154,31 +96,22 @@ sender ( void )
 
       wait_handler = 1;
     }
-  end = __rdtsc ();
-
-  uint64_t avg = ( end - start ) / RUNS;
-  printf ( "Average: %lu (%.2f us)", avg, cycles2us ( avg ) );
 
   worker_stop = 1;
 
   for ( unsigned int i = 0; i < RUNS; i++ )
     samples[i] = tsc_ends[i] - tsc_starts[i];
 
-  print ( samples, RUNS, "Sender" );
-  print ( tsc_sender_worker, i_handler, "Sender/Worker" );
-  print ( tsc_worker, i_handler, "Worker" );
-
-  printf ( "\nSignals handled: %d\n", i_handler );
+  print ( samples, RUNS, "Sender", 0 );
+  print ( tsc_sender_worker, i_handler, "Sender/Worker", 0 );
+  print ( tsc_worker, i_handler, "Worker", 0 );
+  print ( timer_frequency, RUNS, "Timer frequency", 1 );
 }
 
-/*
- * argv[1]: num threads to sending signal
- * argv[2]: microseconds interval misalign between threads to send signal
- */
 int
 main ( int argc, char **argv )
 {
-  cycles_by_us = get_tsc_freq () / 1000000;
+  init_util ();
 
   printf ( "Samples: %lu\n"
            "Cycles by us: %u\n",
@@ -197,6 +130,7 @@ main ( int argc, char **argv )
   sender ();
 
   pthread_join ( tid, NULL );
+  printf ( "\nSignals handled: %d\n", i_handler );
 
   return 0;
 }
