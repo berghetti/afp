4847c9ca8595f8967c002796a8be490f22e40b38
Mayco S. Berghetti <mayco.s.berghetti@gmail.com>
2023-06-29 01:00:42 +0000

    add colors

--- src/debug.h
+++ src/debug.h
@@ -2,17 +2,34 @@
 #ifndef ERROR_H
 #define ERROR_H
 
-#include <errno.h>  // variable errno
+#include <errno.h>
 #include <stdarg.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>  // strerror
+#include <string.h>
 
+#define ANSI_COLOR
+
+#ifdef ANSI_COLOR
+#define RED "\x1b[31;1m"
+#define GREEN "\x1b[32;1m"
+#define YELLOW "\x1b[33;1m"
+#define BLUE "\x1b[34;1m"
+#define MAGENTA "\x1b[35;1m"
+#define RESET "\x1b[0;0m"
+
+#define _INFO GREEN "[INFO] " RESET
+#define _WARNING YELLOW "[WARNING] " RESET
+#define _ERROR MAGENTA "[ERROR] " RESET
+#define _FATAL RED "[FATAL] " RESET
+#define _DEBUG BLUE "[DEBUG] " RESET
+#else
 #define _INFO "[INFO] "
 #define _WARNING "[WARNING] "
 #define _DEBUG "[DEBUG] "
 #define _ERROR "[ERROR] "
 #define _FATAL "[FATAL] "
+#endif
 
 void
 print ( const char *, ... );
@@ -20,14 +37,14 @@ print ( const char *, ... );
 #define INFO( fmt, ... )                          \
   do                                              \
     {                                             \
-      fprintf ( stderr, _INFO fmt, __VA_ARGS__ ); \
+      fprintf ( stdout, _INFO fmt, __VA_ARGS__ ); \
     }                                             \
   while ( 0 )
 
 #define WARNING( fmt, ... )                          \
   do                                                 \
     {                                                \
-      fprintf ( stderr, _WARNING fmt, __VA_ARGS__ ); \
+      fprintf ( stdout, _WARNING fmt, __VA_ARGS__ ); \
     }                                                \
   while ( 0 )
 
