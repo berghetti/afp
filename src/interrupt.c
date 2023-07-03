545455aae74f9f1e091e1bd811579c2e7e42923c
Mayco S. Berghetti <mayco.s.berghetti@gmail.com>
2023-07-03 20:30:44 +0000

    add signal and kmod_ipi types of interruptions

--- src/interrupt.c
+++ src/interrupt.c
@@ -1,12 +1,45 @@
 
 #define _GNU_SOURCE  // tgkill
-#include <stdint.h>
 
-#include <rte_malloc.h>
+#include <stdint.h>
+#include <signal.h>
+#include <sys/syscall.h>
+#include <unistd.h>
 
 #include "afp_internal.h"
+#include "globals.h"
 #include "debug.h"
 #include "../kmod/trap.h"
+
+#ifdef SIGNAL
+
+static pid_t workers_tids[MAX_WORKERS];
+static pid_t pid;
+
+void
+interrupt_send ( uint16_t worker_id )
+{
+  pid_t tid = workers_tids[worker_id];
+  syscall ( SYS_tgkill, pid, tid, SIGUSR1 );
+}
+
+void
+interrupt_register_worker ( uint16_t worker_id, pid_t tid )
+{
+  workers_tids[worker_id] = tid;
+}
+
+void
+interrupt_init ( void ( *handler ) ( int ) )
+{
+  const struct sigaction sa = { .sa_handler = handler, .sa_flags = SA_NODEFER };
+
+  sigaction ( SIGUSR1, &sa, NULL );
+
+  pid = getpid ();
+}
+
+#else
 
 static int workers_core[MAX_WORKERS];
 
@@ -29,3 +62,5 @@ interrupt_init ( user_handler handler )
   if ( trap_init ( handler ) )
     FATAL ( "%s\n", "Error to open kmod_ipi\n" );
 }
+
+#endif
